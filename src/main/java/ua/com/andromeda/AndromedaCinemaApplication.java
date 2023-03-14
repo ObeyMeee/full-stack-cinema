2164c2559ce979ea308147015d87d68f5dcebca5
Andromeda <geraskin1997@gmail.com>
2023-03-14 15:05:36 +0200

    init

---
+++ src/main/java/ua/com/andromeda/AndromedaCinemaApplication.java
@@ -0,0 +1,13 @@
+package ua.com.andromeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndromedaCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndromedaCinemaApplication.class, args);
+    }
+
+}
