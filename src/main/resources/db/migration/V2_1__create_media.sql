aa317c7560a96eae3d7f54ea5bee73d88498ddfb
Andromeda <geraskin1997@gmail.com>
2023-04-04 15:25:46 +0300

    Database changes: update images of films

--- src/main/resources/db/migration/V2_1__create_media.sql
+++ src/main/resources/db/migration/V2_1__create_media.sql
@@ -1,23 +1,24 @@
 INSERT INTO public.media (id, image, trailer)
 VALUES ('1bc2f315-e393-431e-8b37-1999c3f0f77f',
-        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C6AB0EDCE8F41882EBBB782B76DD4F05D7E360D7C3F23B4F6D02C24699B26105/scale?width=1200&aspectRatio=1.78&format=jpeg',
+        'https://wallpapercave.com/wp/wp8660850.jpg',
         'https://www.youtube.com/embed/naQr0uTrH_s');
 INSERT INTO public.media (id, image, trailer)
 VALUES ('263646ad-e642-43b2-85ba-21f8dcd4b27e',
-        'https://upload.wikimedia.org/wikipedia/en/7/7a/Harry_Potter_and_the_Philosopher%27s_Stone_banner.jpg',
+        'https://wallpaperaccess.com/full/4383854.jpg',
         'https://www.youtube.com/embed/VyHV0BRtdxo');
 INSERT INTO public.media (id, image, trailer)
 VALUES ('c3ef119e-92b6-45b1-991d-634e7a849714',
-        'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
+        'https://c4.wallpaperflare.com/wallpaper/1022/525/87/movies-the-lord-of-the-rings-aragorn-viggo-mortensen-movie-posters-posters-the-return-of-the-king-entertainment-movies-hd-art-wallpaper-preview.jpg',
         'https://www.youtube.com/embed/zckJCxYxn1g');
 INSERT INTO public.media (id, image, trailer)
-VALUES ('0aa0d281-33c4-4aa1-a2ae-eca6143c9b6c', 'https://content1.rozetka.com.ua/goods/images/big/236096211.jpg',
+VALUES ('0aa0d281-33c4-4aa1-a2ae-eca6143c9b6c',
+        'https://i.pinimg.com/736x/f2/d6/06/f2d606fbac5792ca44e7d0c7b2c8424b.jpg',
         'https://www.youtube.com/embed/jEDaVHmw7r4');
 INSERT INTO public.media (id, image, trailer)
 VALUES ('40a024a3-c770-4916-ba9b-1738fd5abf77',
-        'https://starwarsblog.starwars.com/wp-content/uploads/2017/05/1-star-wars-image.jpg',
+        'https://wallpapercave.com/dwp1x/wp4932051.jpg',
         'https://www.youtube.com/embed/i-vsILeJ8_8');
 INSERT INTO public.media (id, image, trailer)
 VALUES ('3b234015-49e1-4a4a-aeb8-96f29e72d31f',
-        'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/59580c0d-e740-4686-8120-5614e5bb174b/d4wcyf1-a6afd786-cf81-4c31-9c75-957f2f8313e0.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU5NTgwYzBkLWU3NDAtNDY4Ni04MTIwLTU2MTRlNWJiMTc0YlwvZDR3Y3lmMS1hNmFmZDc4Ni1jZjgxLTRjMzEtOWM3NS05NTdmMmY4MzEzZTAucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.bwPcbV8zbktylxtkzCkR7PhRO2eqfd_h_0I-FnNuxjs',
+        'https://c4.wallpaperflare.com/wallpaper/178/887/600/2008-dark-city-fire-wallpaper-preview.jpg',
         'https://www.youtube.com/embed/g8evyE9TuYk');
