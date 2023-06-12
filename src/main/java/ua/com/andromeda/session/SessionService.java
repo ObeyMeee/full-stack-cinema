d20f1ff7a0c1cbbb9cbc432fe43025d27a33b391
Andromeda <geraskin19970309@gmail.com>
2023-06-12 14:04:23 +0300

    remove unnecessary method

--- src/main/java/ua/com/andromeda/session/SessionService.java
+++ src/main/java/ua/com/andromeda/session/SessionService.java
@@ -1,7 +1,6 @@
 package ua.com.andromeda.session;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
 import ua.com.andromeda.session.dto.SessionBuyTicketDto;
 import ua.com.andromeda.session.dto.SessionDto;
@@ -9,30 +8,14 @@ import ua.com.andromeda.ticket.TicketDto;
 import ua.com.andromeda.ticket.TicketRepository;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SessionService {
     private final SessionRepository sessionRepository;
     private final TicketRepository ticketRepository;
-
-    public Map<UUID, List<SessionDto>> getSessions(boolean enabled) {
-        Streamable<Session> sessions = sessionRepository.findAllByEnabledEquals(enabled);
-        return sessions.stream()
-                .collect(Collectors.toMap(
-                        session -> session.getFilm().getId(),
-                        session ->
-                                sessions.stream()
-                                        .filter(s -> session.getFilm().getId().equals(s.getFilm().getId()))
-                                        .map(SessionDto::new)
-                                        .toList(),
-                        (s1, s2) -> s1
-                ));
-    }
 
     public List<SessionDto> getSessionByFilmId(UUID filmId) {
         return sessionRepository.findAllByFilmId(filmId)
