8f10af5c467bb1fe86e9df39fb3ba578cb2868b0
Andromeda <geraskin19970309@gmail.com>
2023-06-19 10:20:17 +0300

    update db: fill comments and reaction tables

--- src/main/resources/db/migration/V1_2__create_tables.sql
+++ src/main/resources/db/migration/V1_2__create_tables.sql
@@ -1,34 +1,25 @@
-create table public.actors
+create table actors
 (
     id        uuid not null
         primary key,
     full_name varchar(255)
 );
 
-alter table public.actors
-    owner to postgres;
-
-create table public.countries
+create table countries
 (
     id   uuid not null
         primary key,
     name varchar(255)
 );
 
-alter table public.countries
-    owner to postgres;
-
-create table public.genres
+create table genres
 (
     id   uuid not null
         primary key,
     name varchar(255)
 );
 
-alter table public.genres
-    owner to postgres;
-
-create table public.halls
+create table halls
 (
     id     uuid    not null
         primary key,
@@ -36,10 +27,7 @@ create table public.halls
     type   varchar(255)
 );
 
-alter table public.halls
-    owner to postgres;
-
-create table public.media
+create table media
 (
     id      uuid not null
         primary key,
@@ -47,10 +35,7 @@ create table public.media
     trailer text
 );
 
-alter table public.media
-    owner to postgres;
-
-create table public.films
+create table films
 (
     id              uuid    not null
         primary key,
@@ -62,13 +47,10 @@ create table public.films
     title           varchar(255),
     media_id        uuid
         constraint fkrdfc2dr0b455fesu2qf11qovo
-            references public.media
+            references media
 );
 
-alter table public.films
-    owner to postgres;
-
-create table public.comments
+create table comments
 (
     id       uuid             not null
         primary key,
@@ -77,52 +59,51 @@ create table public.comments
     username varchar(255),
     film_id  uuid             not null
         constraint fkpiw1eyj5u8hwd162g9kgj3vuc
-            references public.films
+            references films
 );
 
-alter table public.comments
-    owner to postgres;
-
-create table public.films_actors
+create table films_actors
 (
     film_id  uuid not null
         constraint fkm871tpbjgvlefqev7aaq827s0
-            references public.films,
+            references films,
     actor_id uuid not null
         constraint fkdjtf3dy8e0s3x13r8noaif9w
-            references public.actors
+            references actors
 );
 
-alter table public.films_actors
-    owner to postgres;
-
-create table public.films_countries
+create table films_countries
 (
     film_id    uuid not null
         constraint fk6tkrwhgida3f5394lkxl7gogp
-            references public.films,
+            references films,
     country_id uuid not null
         constraint fkr4qe9orc9gf3qnioq1fj1wgxq
-            references public.countries
+            references countries
 );
 
-alter table public.films_countries
-    owner to postgres;
-
-create table public.films_genres
+create table films_genres
 (
     film_id  uuid not null
         constraint fkqr8m71obccc9w6cp91l3k8r2w
-            references public.films,
+            references films,
     genre_id uuid not null
         constraint fktcwy3ocjyhnni2yr22y2hpb9p
-            references public.genres
+            references genres
 );
 
-alter table public.films_genres
-    owner to postgres;
+create table reactions
+(
+    id         bigserial
+        primary key,
+    type       varchar(255),
+    username   varchar(255),
+    comment_id uuid
+        constraint fk49rifnmyo1sd243acaysemlbw
+            references comments
+);
 
-create table public.rows
+create table rows
 (
     id      uuid    not null
         primary key,
@@ -131,26 +112,20 @@ create table public.rows
     type    varchar(255),
     hall_id uuid
         constraint fk8dwpdpkba6x7g4qbgg1ic0ik7
-            references public.halls
+            references halls
 );
 
-alter table public.rows
-    owner to postgres;
-
-create table public.seats
+create table seats
 (
     id     uuid    not null
         primary key,
     number integer not null,
     row_id uuid
         constraint fkfi3owtwwqbc0mh605hq2ygs1x
-            references public.rows
+            references rows
 );
 
-alter table public.seats
-    owner to postgres;
-
-create table public.sessions
+create table sessions
 (
     id       uuid    not null
         primary key,
@@ -158,16 +133,13 @@ create table public.sessions
     start_at timestamp(6),
     film_id  uuid    not null
         constraint fkn2m0d43s7i2gofapl0d8qkvq7
-            references public.films,
+            references films,
     hall_id  uuid    not null
         constraint fkcbrgca6k34wv4jr41ik2qdoaf
-            references public.halls
+            references halls
 );
 
-alter table public.sessions
-    owner to postgres;
-
-create table public.tickets
+create table tickets
 (
     id         uuid    not null
         primary key,
@@ -177,9 +149,6 @@ create table public.tickets
     seat       integer not null,
     username   varchar(255),
     session_id uuid    not null
-        constraint fk6t5777nsc5ela5uwyh9ot3a0w
-            references public.sessions
+        constraint fk6yhwfajgdoqa8kq4gnuimtkpp
+            references sessions
 );
-
-alter table public.tickets
-    owner to postgres;
