f14962f4957bbc76d12acea58d810ba7a178b870
Andromeda <geraskin19970309@gmail.com>
2023-09-18 15:30:01 +0300

    db change: add countries to films

--- src/main/resources/db/migration/V2_2__create_countries.sql
+++ src/main/resources/db/migration/V2_2__create_countries.sql
@@ -1,6 +1,4 @@
-INSERT INTO public.countries (id, name)
-VALUES ('d6ee0892-c51b-448f-99d9-a79133a47535', 'New Zealand');
-INSERT INTO public.countries (id, name)
-VALUES ('cbe685d1-1239-4a37-8a2a-31e66ae1b187', 'UK');
-INSERT INTO public.countries (id, name)
-VALUES ('b4a72e57-f891-471d-aea3-4ef69780607f', 'USA');
+INSERT INTO public.countries (id, name) VALUES ('d6ee0892-c51b-448f-99d9-a79133a47535', 'New Zealand');
+INSERT INTO public.countries (id, name) VALUES ('cbe685d1-1239-4a37-8a2a-31e66ae1b187', 'UK');
+INSERT INTO public.countries (id, name) VALUES ('b4a72e57-f891-471d-aea3-4ef69780607f', 'USA');
+INSERT INTO public.countries (id, name) VALUES ('314215b5-08ed-427c-ad09-ed0048ad98e0', 'Germany');
---
+++ src/main/resources/db/migration/V3_4__create_film_countries.sql
@@ -0,0 +1,23 @@
+INSERT INTO public.films_countries(film_id, country_id)
+VALUES
+    -- The Lord of the Rings: The Return of the King
+    ('2073d25d-bf80-46d0-9c20-da96dd8cbaad', 'd6ee0892-c51b-448f-99d9-a79133a47535'),
+    ('2073d25d-bf80-46d0-9c20-da96dd8cbaad', 'cbe685d1-1239-4a37-8a2a-31e66ae1b187'),
+    ('2073d25d-bf80-46d0-9c20-da96dd8cbaad', 'b4a72e57-f891-471d-aea3-4ef69780607f'),
+
+    -- Dark knight rises
+    ('303e7f44-4606-4848-ab3c-b8ba767ea526', 'cbe685d1-1239-4a37-8a2a-31e66ae1b187'),
+    ('303e7f44-4606-4848-ab3c-b8ba767ea526', 'b4a72e57-f891-471d-aea3-4ef69780607f'),
+
+    -- Pirates of the Caribbean: The Curse  of the Black Pearl
+    ('dba5a1bb-454e-484c-8695-48010c99a57a', 'b4a72e57-f891-471d-aea3-4ef69780607f'),
+
+    -- Home alone
+    ('203c2490-f370-4684-a7cd-7290ceeddae0', 'b4a72e57-f891-471d-aea3-4ef69780607f'),
+
+    -- Harry Potter and the Philosopher's stone
+    ('4108bc1f-cc23-422c-babd-3e19be79ac05', 'cbe685d1-1239-4a37-8a2a-31e66ae1b187'),
+    ('4108bc1f-cc23-422c-babd-3e19be79ac05', 'b4a72e57-f891-471d-aea3-4ef69780607f'),
+
+    -- Star Wars
+    ('37262b52-d1b4-420d-ae3a-fc32574da793', 'b4a72e57-f891-471d-aea3-4ef69780607f');
